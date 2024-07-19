bcdecab699c7513d0ddf444c4ea254724efdfbf4
Jim Phillips <jim@oberon.group>
2024-07-19 18:34:16 +0000

    Removed phone from footer

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -2,9 +2,6 @@
   <p>
     <i class="far fa-envelope-open" />
     <a href="mailto:info@ptycoin.com">info@ptycoin.com</a>
-    |
-    <i class="fab fa-whatsapp" />
-    <a href="https://api.whatsapp.com/send?phone=50765709999">+507 6570-9999</a>
   </p>
   <p>&copy; 2024 PTYcoin.com - Todos los derechos reservados.</p>
 </footer>
