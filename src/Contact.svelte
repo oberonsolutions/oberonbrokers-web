efe97f454608df897e34a4796a462a6d3747b2c8
Jim Phillips <jim@oberon.group>
2024-07-27 16:34:21 +0000

    Adjusting farewell message for complete shutdown.

--- src/Contact.svelte
+++ src/Contact.svelte
@@ -3,31 +3,8 @@
   export let small = false;
 </script>
 
-{#if small}
-  <Container fluid class="rounded bg-light p-3 mb-1 mb-md-3 d-md-none text-center">
-    Brokers de Criptomonedas | 
-    <span class="text-nowrap">
-      <i class="far fa-envelope-open" />
-      <a href="mailto:info@ptycoin.com">info@ptycoin.com</a>
-    </span>
-  </Container>
-{:else}
+{#if !small}
   <div class="card">
     <img class="card-img-top bg-light" src="img/ptycoin.jpg" alt="PTYcoin" />
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item">PTYcoin, Marca de Oberon Brokers</li>
-      <li class="list-group-item">Brokers de Criptomonedas</li>
-      <li class="list-group-item">
-        <span class="flag-icon flag-icon-pa" />
-        Panamá, Republica de Panamá
-        <br />
-      </li>
-      <li class="list-group-item">
-        <span class="text-nowrap">
-          <i class="far fa-envelope-open" />
-          <a href="mailto:info@ptycoin.com">info@ptycoin.com</a>
-        </span>
-      </li>
-    </ul>
   </div>
 {/if}
